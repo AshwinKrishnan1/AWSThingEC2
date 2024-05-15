2feb8727d88a01d994d345303d5ede389d5c24f6
AshwinKrishnan1 <128541133+AshwinKrishnan1@users.noreply.github.com>
2024-05-14 21:49:37 -0500

    Update index.js

--- news3thing/index.js
+++ news3thing/index.js
@@ -1,25 +1,25 @@
-process.env[‘PATH’] = process.env[‘PATH’] + ‘:’ + process.env[‘LAMBDA_TASK_ROOT’];
-
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
-const { spawn } = require('child_process');
+const process = require('child_process');
 const fs = require('fs');
 const path = require('path');
 const os = require('os');
+const { spawn } = process;
 
+process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
 
 
 exports.handler = async (event) => {
     // Running the executable
     try {
-        const process = spawn(helloWorld, []);
-        process.stdout.on('data', (data) => {
+        const child = spawn(helloWorld, []);
+        child.stdout.on('data', (data) => {
             console.log(`stdout: ${data}`);
         });
-        process.stderr.on('data', (data) => {
+        child.stderr.on('data', (data) => {
             console.error(`stderr: ${data}`);
         });
-        process.on('close', (code) => {
+        child.on('close', (code) => {
             console.log(`child process exited with code ${code}`);
         });
     } catch (error) {
